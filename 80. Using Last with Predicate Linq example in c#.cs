bb0c60e7b04852e4de284439e7b7d9be03371c8c
Sandeep Pal <sandeep084991904@gmail.com>
2024-10-11 13:19:21 +0530

    Create 80. Using Last with Predicate Linq example in c#.cs

---
+++ 80. Using Last with Predicate Linq example in c#.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    static void Main()
+    {
+        var products = new List<Product>
+        {
+            new Product { Name = "Laptop", Price = 1000m },
+            new Product { Name = "Mouse", Price = 20m },
+            new Product { Name = "Keyboard", Price = 50m }
+        };
+
+        var lastCheapProduct = products.Last(p => p.Price < 100);
+
+        Console.WriteLine($"Last Cheap Product: {lastCheapProduct.Name}");
+    }
+}
