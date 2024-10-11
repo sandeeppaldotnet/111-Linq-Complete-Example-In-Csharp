06a5f4a4738eeb216d4d24d0bfed7051659f3275
Sandeep Pal <sandeep084991904@gmail.com>
2024-10-11 11:32:02 +0530

    Create select-projection-squares-numbers-linq-query.cs

---
+++ select-projection-squares-numbers-linq-query.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var squares = numbers.Select(n => n * n);
+        Console.WriteLine("Squares:");
+        squares.ToList().ForEach(n => Console.WriteLine(n));
+    }
+}
